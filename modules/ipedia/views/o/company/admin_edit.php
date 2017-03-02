f70923a178bf58f5bcd55df6554492c18ebb7ef3
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-03-02 16:44:43 +0700

    company: add crud source

---
+++ modules/ipedia/views/o/company/admin_edit.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Ipedia Companies (ipedia-companies)
+ * @var $this CompanyController
+ * @var $model IpediaCompanies
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @created date 2 March 2017, 16:41 WIB
+ * @link https://github.com/ommu/iPedia
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Ipedia Companies'=>array('manage'),
+		$model->company_id=>array('view','id'=>$model->company_id),
+		'Update',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
