390479854546fc4f8ac9ea73db379ba15ddf04c7
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-17 21:30:04 +0700

    no message

--- modules/ipedia/models/IpediaPositions.php
+++ modules/ipedia/models/IpediaPositions.php
@@ -301,7 +301,7 @@ class IpediaPositions extends CActiveRecord
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
+				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'creation_date',
 					'language' => 'en',
--- modules/ipedia/models/IpediaSetting.php
+++ modules/ipedia/models/IpediaSetting.php
@@ -224,7 +224,7 @@ class IpediaSetting extends CActiveRecord
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
+				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'modified_date',
 					'language' => 'en',
