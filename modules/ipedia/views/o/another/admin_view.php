e033b6f7263bbb1fa196488348126ab01d088055
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-19 10:27:43 +0700

    another: fixed gridview and crud

---
+++ modules/ipedia/views/o/another/admin_view.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Ipedia Anothers (ipedia-anothers)
+ * @var $this AnotherController
+ * @var $model IpediaAnothers
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @created date 19 April 2017, 09:50 WIB
+ * @link http://opensource.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Ipedia Anothers'=>array('manage'),
+		$model->another_id,
+	);
+?>
+
+<div class="dialog-content">
+	<?php $this->widget('application.components.system.FDetailView', array(
+		'data'=>$model,
+		'attributes'=>array(
+			array(
+				'name'=>'another_id',
+				'value'=>$model->another_id,
+			),
+			array(
+				'name'=>'publish',
+				'value'=>$model->publish == '1' ? Chtml::image(Yii::app()->theme->baseUrl.'/images/icons/publish.png') : Chtml::image(Yii::app()->theme->baseUrl.'/images/icons/unpublish.png'),
+				'type'=>'raw',
+			),
+			array(
+				'name'=>'another_name',
+				'value'=>$model->another_name != '' ? $model->another_name : '-',
+			),
+			array(
+				'name'=>'creation_date',
+				'value'=>!in_array($model->creation_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00')) ? Utility::dateFormat($model->creation_date, true) : '-',
+			),
+			array(
+				'name'=>'creation_id',
+				'value'=>$model->creation_id != 0 ? $model->creation->displayname : '-',
+			),
+			array(
+				'name'=>'modified_date',
+				'value'=>!in_array($model->modified_date, array('0000-00-00 00:00:00','1970-01-01 00:00:00')) ? Utility::dateFormat($model->modified_date, true) : '-',
+			),
+			array(
+				'name'=>'modified_id',
+				'value'=>$model->modified_id != 0 ? $model->modified->displayname : '-',
+			),
+		),
+	)); ?>
+</div>
+<div class="dialog-submit">
+	<?php echo CHtml::button(Yii::t('phrase', 'Close'), array('id'=>'closed')); ?>
+</div>
