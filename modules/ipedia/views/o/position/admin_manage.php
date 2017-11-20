95feeaeae1cac0c575e7473e123b9a37f4c93409
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-20 20:50:26 +0700

    modify component system location nb. module core path condition

--- modules/ipedia/views/o/position/admin_manage.php
+++ modules/ipedia/views/o/position/admin_manage.php
@@ -95,7 +95,7 @@
 				'template' => '{view}|{update}|{delete}',
 			));
 
-			$this->widget('application.components.system.OGridView', array(
+			$this->widget('application.libraries.core.components.system.OGridView', array(
 				'id'=>'ipedia-positions-grid',
 				'dataProvider'=>$model->search(),
 				'filter'=>$model,
