cfb14e617a9295eed87635aa805c832dda8dc23e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-06-28 14:13:26 +0700

    add count column condition in getInfo function

--- models/IpediaDirectories.php
+++ models/IpediaDirectories.php
@@ -353,13 +353,16 @@ class IpediaDirectories extends CActiveRecord
 	{
 		if($column != null) {
 			$model = self::model()->findByPk($id,array(
-				'select' => $column
+				'select' => $column,
 			));
-			return $model->$column;
+			if(count(explode(',', $column)) == 1)
+				return $model->$column;
+			else
+				return $model;
 			
 		} else {
 			$model = self::model()->findByPk($id);
-			return $model;			
+			return $model;
 		}
 	}
 
--- models/ViewIpediaDirectoryLocation.php
+++ models/ViewIpediaDirectoryLocation.php
@@ -199,13 +199,16 @@ class ViewIpediaDirectoryLocation extends CActiveRecord
 	{
 		if($column != null) {
 			$model = self::model()->findByPk($id,array(
-				'select' => $column
+				'select' => $column,
 			));
-			return $model->$column;
+ 			if(count(explode(',', $column)) == 1)
+ 				return $model->$column;
+ 			else
+ 				return $model;
 			
 		} else {
 			$model = self::model()->findByPk($id);
-			return $model;			
+			return $model;
 		}
 	}
 
