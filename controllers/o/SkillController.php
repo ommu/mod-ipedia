7a20bc5acfb6f1ebac5a1270cb69a008c0ed4e11
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-22 00:54:42 +0700

    change Runaction action

--- controllers/o/SkillController.php
+++ controllers/o/SkillController.php
@@ -13,7 +13,7 @@
  *	Add
  *	Edit
  *	View
- *	RunAction
+ *	Runaction
  *	Delete
  *	Publish
  *
@@ -303,7 +303,7 @@ class SkillController extends Controller
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
 	 */
-	public function actionRunAction() {
+	public function actionRunaction() {
 		$id       = $_POST['trash_id'];
 		$criteria = null;
 		$actions  = Yii::app()->getRequest()->getParam('action');
--- controllers/o/UniversitymajorController.php
+++ controllers/o/UniversitymajorController.php
@@ -11,7 +11,7 @@
  *	Add
  *	Manage
  *	View
- *	RunAction
+ *	Runaction
  *	Delete
  *	Publish
  *
@@ -190,7 +190,7 @@ class UniversitymajorController extends Controller
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
 	 */
-	public function actionRunAction() {
+	public function actionRunaction() {
 		$id       = $_POST['trash_id'];
 		$criteria = null;
 		$actions  = Yii::app()->getRequest()->getParam('action');
