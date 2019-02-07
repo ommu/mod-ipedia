0f233d0441bf97ff9c3c9533fb384e3ea42eec4c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-02-07 19:57:47 +0700

    module config

---
+++ Module.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ipedia module definition class
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
+ * @created date 7 February 2019, 19:53 WIB
+ * @link https://github.com/ommu/mod-ipedia
+ *
+ */
+
+namespace ommu\ipedia;
+
+class Module extends \app\components\Module
+{
+	public $layout = 'main';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public $controllerNamespace = 'ommu\ipedia\controllers';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function init()
+	{
+		parent::init();
+
+		// custom initialization code goes here
+	}
+}
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ipedia module config
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
+ * @created date 7 February 2019, 19:53 WIB
+ * @link https://github.com/ommu/mod-ipedia
+ *
+ */
+
+return [
+	'id' => 'ipedia',
+	'class' => ommu\ipedia\Module::className(),
+];
