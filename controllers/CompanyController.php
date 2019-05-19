8be3b5461c14caf44f2ce58597d46d481408bab6
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-19 21:42:05 +0700

    commit

--- controllers/CompanyController.php
+++ controllers/CompanyController.php
@@ -141,6 +141,7 @@ class CompanyController extends Controller
 	public function actionUpdate($id)
 	{
 		$model = $this->findModel($id);
+
 		if(Yii::$app->request->isPost) {
 			$model->load(Yii::$app->request->post());
 			// $postData = Yii::$app->request->post();
