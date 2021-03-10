bf2e2478cb9e66453860548755d1a9d0762084db
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2021-03-10 15:53:49 +0700

    psr4 drop yiisoft/yii2 on composer

--- views/company/admin_view.php
+++ views/company/admin_view.php
@@ -23,7 +23,7 @@ $this->params['breadcrumbs'][] = $model->company_name;
 
 if (!$small) {
     $this->params['menu']['content'] = [
-        ['label' => Yii::t('app', 'Detail'), 'url' => Url::to(['view', 'id' => $model->company_id]), 'icon' => 'eye', 'htmlOptions' => ['class' => 'btn btn-success']],
+        ['label' => Yii::t('app', 'Detail'), 'url' => Url::to(['view', 'id' => $model->company_id]), 'icon' => 'eye', 'htmlOptions' => ['class' => 'btn btn-info']],
         ['label' => Yii::t('app', 'Update'), 'url' => Url::to(['update', 'id' => $model->company_id]), 'icon' => 'pencil', 'htmlOptions' => ['class' => 'btn btn-primary']],
         ['label' => Yii::t('app', 'Delete'), 'url' => Url::to(['delete', 'id' => $model->company_id]), 'htmlOptions' => ['data-confirm' => Yii::t('app', 'Are you sure you want to delete this item?'), 'data-method' => 'post', 'class' => 'btn btn-danger'], 'icon' => 'trash'],
     ];
@@ -31,77 +31,80 @@ if (!$small) {
 
 <div class="ipedia-companies-view">
 
-<?php echo DetailView::widget([
+<?php
+$attributes = [
+    'company_id',
+    [
+        'attribute' => 'publish',
+        'value' => in_array($model->publish, [0,1]) ? $model->quickAction(Url::to(['publish', 'id' => $model->primaryKey]), $model->publish) : IpediaCompanies::getPublish($model->publish),
+        'format' => 'raw',
+    ],
+    [
+        'attribute' => 'memberDisplayname',
+        'value' => function ($model) {
+            $memberDisplayname = isset($model->member) ? $model->member->displayname : '-';
+            if ($memberDisplayname != '-') {
+                return Html::a($memberDisplayname, ['/member/manage/admin/view', 'id' => $model->member_id], ['title' => $memberDisplayname]);
+            }
+            return $memberDisplayname;
+        },
+        'format' => 'html',
+    ],
+    [
+        'attribute' => 'company_name',
+        'value' => $model->company_name ? $model->company_name : '-',
+    ],
+    [
+        'attribute' => 'creation_date',
+        'value' => Yii::$app->formatter->asDatetime($model->creation_date, 'medium'),
+        'visible' => !$small,
+    ],
+    [
+        'attribute' => 'creationDisplayname',
+        'value' => isset($model->creation) ? $model->creation->displayname : '-',
+        'visible' => !$small,
+    ],
+    [
+        'attribute' => 'modified_date',
+        'value' => Yii::$app->formatter->asDatetime($model->modified_date, 'medium'),
+        'visible' => !$small,
+    ],
+    [
+        'attribute' => 'modifiedDisplayname',
+        'value' => isset($model->modified) ? $model->modified->displayname : '-',
+        'visible' => !$small,
+    ],
+    [
+        'attribute' => 'updated_date',
+        'value' => Yii::$app->formatter->asDatetime($model->updated_date, 'medium'),
+        'visible' => !$small,
+    ],
+    [
+        'attribute' => 'industries',
+        'value' => function ($model) {
+            $industries = $model->getIndustries(true);
+            return Html::a($industries, ['company-industry/manage', 'company' => $model->primaryKey, 'publish' => 1], ['title' => Yii::t('app', '{count} industries', ['count' => $industries])]);
+        },
+        'format' => 'html',
+        'visible' => !$small,
+    ],
+    [
+        'attribute' => 'universities',
+        'value' => function ($model) {
+            $universities = $model->getUniversities(true);
+            return Html::a($universities, ['university/manage', 'company' => $model->primaryKey, 'publish' => 1], ['title' => Yii::t('app', '{count} universities', ['count' => $universities])]);
+        },
+        'format' => 'html',
+        'visible' => !$small,
+    ],
+];
+
+echo DetailView::widget([
 	'model' => $model,
 	'options' => [
 		'class' => 'table table-striped detail-view',
 	],
-	'attributes' => [
-		'company_id',
-		[
-			'attribute' => 'publish',
-			'value' => in_array($model->publish, [0,1]) ? $model->quickAction(Url::to(['publish', 'id' => $model->primaryKey]), $model->publish) : IpediaCompanies::getPublish($model->publish),
-			'format' => 'raw',
-		],
-		[
-			'attribute' => 'memberDisplayname',
-			'value' => function ($model) {
-				$memberDisplayname = isset($model->member) ? $model->member->displayname : '-';
-                if ($memberDisplayname != '-') {
-                    return Html::a($memberDisplayname, ['/member/manage/admin/view', 'id' => $model->member_id], ['title' => $memberDisplayname]);
-                }
-				return $memberDisplayname;
-			},
-			'format' => 'html',
-		],
-		[
-			'attribute' => 'company_name',
-			'value' => $model->company_name ? $model->company_name : '-',
-		],
-		[
-			'attribute' => 'creation_date',
-			'value' => Yii::$app->formatter->asDatetime($model->creation_date, 'medium'),
-			'visible' => !$small,
-		],
-		[
-			'attribute' => 'creationDisplayname',
-			'value' => isset($model->creation) ? $model->creation->displayname : '-',
-			'visible' => !$small,
-		],
-		[
-			'attribute' => 'modified_date',
-			'value' => Yii::$app->formatter->asDatetime($model->modified_date, 'medium'),
-			'visible' => !$small,
-		],
-		[
-			'attribute' => 'modifiedDisplayname',
-			'value' => isset($model->modified) ? $model->modified->displayname : '-',
-			'visible' => !$small,
-		],
-		[
-			'attribute' => 'updated_date',
-			'value' => Yii::$app->formatter->asDatetime($model->updated_date, 'medium'),
-			'visible' => !$small,
-		],
-		[
-			'attribute' => 'industries',
-			'value' => function ($model) {
-				$industries = $model->getIndustries(true);
-				return Html::a($industries, ['company-industry/manage', 'company' => $model->primaryKey, 'publish' => 1], ['title' => Yii::t('app', '{count} industries', ['count' => $industries])]);
-			},
-			'format' => 'html',
-			'visible' => !$small,
-		],
-		[
-			'attribute' => 'universities',
-			'value' => function ($model) {
-				$universities = $model->getUniversities(true);
-				return Html::a($universities, ['university/manage', 'company' => $model->primaryKey, 'publish' => 1], ['title' => Yii::t('app', '{count} universities', ['count' => $universities])]);
-			},
-			'format' => 'html',
-			'visible' => !$small,
-		],
-	],
+	'attributes' => $attributes,
 ]); ?>
 
 </div>
